e416ea5d2f041dd8ec2a6bf119dae35e07b1f4e1
sri85 <sripathi@roxtr.com>
2016-06-14 12:47:16 +0100

    Fixed the logic problem

--- searching_sorting_algorithms/binary_search_with_iteration.py
+++ searching_sorting_algorithms/binary_search_with_iteration.py
@@ -1,14 +1,18 @@
 def binary_search(l,item):
-    first = 0
-    last = len(l)-1
+        first = 0
+        last = len(l)-1
 
-    while first < = last:
-        midpoint = (first+last)//2
-        if l[midpoint] == item:
-            return l.index(item)
-        else:
-            if l[midpoint] < item:
-                last = l[midpoint]-1
+        while first<=last:
+            midpoint = (first + last)//2
+            if l[midpoint] == item:
+                return l.index(item)
             else:
-                first = l[midpoint]+1
-    return -1
+                if item < l[midpoint]:
+                    last = midpoint-1
+                else:
+                    first = midpoint+1
+
+        return -1
+
+assert binary_search([1,2,6,7,10,11,45,78,89],3) == -1
+assert binary_search([1,2,6,7,10,11,45,78,89],45) == 6
